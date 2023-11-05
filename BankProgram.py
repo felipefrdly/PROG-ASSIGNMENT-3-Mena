02ce65b3b33a04da44fd805f3721e4f753e11b0b
felipefrdly <104601475+felipefrdly@users.noreply.github.com>
2023-11-05 13:23:39 -0500

    Created Skeleton
    Planned out what the methods in application are going to looklike

---
+++ BankProgram.py
@@ -0,0 +1,18 @@
+"""
+Author: Felipe Mena
+Date: 05/11/2023
+Description:
+"""
+
+class Application:
+    def run(self):
+        #TODO When this method is called run the other class methods
+        pass
+
+    def showMainMenu(self):
+        #TODO When this method is called that displays the options: Select Account, Open Account, and Exit
+        pass
+
+    def showAccountMenu(self):
+        #TODO When this method is called display the options: Check Balence, Deposit, Withdraw, and Exit Account
+        pass
